54be1176b4b3475280646a99ef5ac2669841e7ea
Ahmedznaty1 <znatyahmed5@gmail.com>
2024-02-10 16:21:50 +0200

    upload

---
+++ src/app/endfooter/endfooter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endfooter',
+  templateUrl: './endfooter.component.html',
+  styleUrls: ['./endfooter.component.css']
+})
+export class EndfooterComponent {
+
+}
